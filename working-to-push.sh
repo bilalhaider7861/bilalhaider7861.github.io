b57feb16cc47ce82b7c6bedb16ff91df4b676a2b
bilalhaider7861 <bilal-haider7861@hotmail.com>
2022-02-08 03:18:27 +0500

    all

--- working-to-push.sh
+++ working-to-push.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+echo "Adding all work done to stagging area"
+
 git add --all 
 
 echo "write a comment before commit and press enter"
+
 read -p 'input: ' inputvar
+
 git commit -m "$inputvar"   
+
 echo "now pushing commit to remote repo"
+
 git push origin main
+
+echo "Completed"
 

47e63a193c118f4eb631788317d8322ad77fc936
bilalhaider7861 <bilal-haider7861@hotmail.com>
2022-02-08 04:00:55 +0500

    changing resume colour to green

--- working-to-push.sh
+++ working-to-push.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
+echo "----------------------------------------"
+echo "----------------------------------------"
+
 echo "Adding all work done to stagging area"
+
+echo "----------------------------------------"
 
 git add --all 
 
+echo "----------------------------------------"
+
 echo "write a comment before commit and press enter"
+
+echo "----------------------------------------"
 
 read -p 'input: ' inputvar
 
+echo "----------------------------------------"
+
 git commit -m "$inputvar"   
+
+echo "----------------------------------------"
 
 echo "now pushing commit to remote repo"
 
+echo "----------------------------------------"
+
 git push origin main
 
+echo "----------------------------------------"
+
 echo "Completed"
+
 
